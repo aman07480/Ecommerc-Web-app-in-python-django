dd184e791e709c5f91fc84fbda54c9ab1b94ba4f
aman tamboli 07 <tamboliaman109@gmail.com>
2026-06-08 17:26:03 +0530

    created new app and written code

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ecommerceapp',
+    'auth',
 
 ]
 
@@ -116,4 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+import os
+
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+
--- auth/urls.py
+++ auth/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from ecommerceapp import views
+from auth import views
 
 urlpatterns = [
-    path("",views.index,name="index"),
+     path("",views.index,name="index"),
     path("contact/",views.contact,name="contact"),
     path("about",views.about,name="about"),
    
